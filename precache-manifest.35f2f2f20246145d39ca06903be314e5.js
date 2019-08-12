6b8ea79c8b5674879e2b17e619159e5066d452ea
akhilomar <akhilomar555@gmail.com>
2019-08-13 02:04:44 +0530

    Updates

---
+++ precache-manifest.35f2f2f20246145d39ca06903be314e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69ef4b3986a3363cac1b63af80a34f0",
+    "url": "/RBC/index.html"
+  },
+  {
+    "revision": "75c19f0bd96fa930ae39",
+    "url": "/RBC/static/css/main.811c92a3.chunk.css"
+  },
+  {
+    "revision": "a2ffd0a6539d3038abb9",
+    "url": "/RBC/static/js/2.25c31974.chunk.js"
+  },
+  {
+    "revision": "75c19f0bd96fa930ae39",
+    "url": "/RBC/static/js/main.20096e92.chunk.js"
+  },
+  {
+    "revision": "88054f1653dc4d80f932",
+    "url": "/RBC/static/js/runtime~main.233face0.js"
+  }
+]);
